9b230d4892e544401fe7615aaf0043e90b4f429b
dengbaole <dengbaole155@gmail.com>
2023-12-17 14:31:02 +0800

    队列

--- main.c
+++ main.c
@@ -3,78 +3,78 @@
 #include <stdlib.h>
 #include <windows.h>
 
-//栈的特点 先进后出
+/// 队列先进后出
 typedef struct Node {
 	int data;
 	struct Node* next;
 } NODE;
 
-NODE* initstack(void) {
-	NODE* S = (NODE*)malloc(sizeof(NODE));
-	S -> data = 0;
-	S -> next = NULL;
-	return S;
+NODE* initqueue(void){
+	NODE* Q = (NODE*)malloc(sizeof(NODE));
+	Q ->data = 0;
+	Q ->next = NULL;
+	return Q;
 }
 
 
-int isempty(NODE* S) {
-	if(S -> next == NULL) {
+int isempty(NODE* Q){
+	if(Q ->next == NULL){
 		return 1;
-	} else {
+	}else{
 		return 0;
 	}
 }
 
-int gitTOP(NODE* S) {
-	if(isempty(S)) {
-		return -1;
-	} else {
-		return S -> next -> data;
+
+/// @brief 
+/// @param Q 
+/// @param data 
+/// @return 
+NODE* enqueue(NODE* Q,int data){
+	NODE* q;
+	NODE* node = (NODE*)malloc(sizeof(NODE));
+	node ->data = data;
+	while(q->next!=NULL){
+		q = q ->next;
 	}
+	node->next = q->next;
+	q ->next = node;
 }
 
-/// @brief 出栈
-/// @param S 
-/// @return 
-int pop(NODE* S) {
-	if(isempty(S)) {
-		return -1;
+
+int delqueue(NODE* Q){
+	if(isempty(Q)){
+		return -1;	
 	}else{
-		NODE* node = S->next;
+		NODE* node = Q ->next;
 		int data = node ->data;
-		S->next = node ->next;
+		Q ->next = node ->next;
 		free(node);
 		return data;
 	}
+
 }
 
-
-void push(NODE* S, int data) {
-	NODE* node = (NODE*)malloc(sizeof(NODE));
-	node ->data = data;
-	node ->next = S ->next;
-	S ->next = node;
+int test(){
+	int i = 0;
+	return i;
 }
-
-
-void printstack(NODE* S) {
-	NODE* node = S ->next;
-	while (node) {
-		printf("%d -> ", node ->data);
-		node = node->next;
+void printqueue(NODE* Q){
+	NODE* node = Q ->next;
+	while (node){
+		printf("%d ->",node->data);
+		node = node ->next;
 	}
-	printf("NULL\n");
+	printf("NULL");
 }
 
-
-int main(void) {
-	NODE* S = initstack();
-	push(S, 1);
-	push(S, 4);
-	push(S, 2);
-	push(S, 3);
-	printstack(S);
-	int i = pop(S);
-	printf("%d \n",i);
+int main(void){
+	NODE* Q = initqueue();
+	enqueue(Q,1);
+	enqueue(Q,3);
+	enqueue(Q,3);
+	enqueue(Q,4);
+	delqueue(Q);
+	printqueue(Q);
 	return 0;
 }
