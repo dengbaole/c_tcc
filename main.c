4a9e189d26be02f5e40e997986ae3d3c769041bd
dengbaole <dengbaole155@gmail.com>
2023-12-17 10:43:36 +0800

    栈

--- main.c
+++ main.c
@@ -3,97 +3,78 @@
 #include <stdlib.h>
 #include <windows.h>
 
-typedef struct node {
+//栈的特点 先进后出
+typedef struct Node {
 	int data;
-    struct node* pre;
-	struct node* next;
+	struct Node* next;
 } NODE;
 
-/*
-1. 初始化的时候list 创建之后不会销毁吗，返回为什么可以    可以返回指针但是不能返回值
-
-*/
-
-NODE* initlist(void) {
-	NODE* list = (NODE*)malloc(sizeof(NODE));
-	list -> data = 0;
-	list -> next = list;
-    list ->pre = list;
-	return list;
-}
-
-void headinsert(NODE* list,int data){
-    NODE* node = (NODE*)malloc(sizeof(NODE));
-    node ->data = data;
-    if(list -> next == NULL){
-        //链表空
-        node->next = list -> next;
-        node -> pre = list;
-        list ->next = node;
-        list ->pre = node;
-    }else{
-        node ->pre = list;
-        node ->next = list->next;
-        list ->next ->pre = node;
-        list ->next = node;
-    }
-    
+NODE* initstack(void) {
+	NODE* S = (NODE*)malloc(sizeof(NODE));
+	S -> data = 0;
+	S -> next = NULL;
+	return S;
 }
 
 
-void tailinsert(NODE* list,int data){
-    NODE* l = list;
-    NODE* node = (NODE*)malloc(sizeof(NODE));
-    node -> data = data;
-    while (l -> next != list){
-        l = l ->next;
-    }
-    l->next = node;
-    node -> next = list;
-    node -> pre = l;
-    list -> pre = node;
+int isempty(NODE* S) {
+	if(S -> next == NULL) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int gitTOP(NODE* S) {
+	if(isempty(S)) {
+		return -1;
+	} else {
+		return S -> next -> data;
+	}
+}
+
+/// @brief 出栈
+/// @param S 
+/// @return 
+int pop(NODE* S) {
+	if(isempty(S)) {
+		return -1;
+	}else{
+		NODE* node = S->next;
+		int data = node ->data;
+		S->next = node ->next;
+		free(node);
+		return data;
+	}
 }
 
 
-int deletelist(NODE* list,int data){
-    NODE* node = list -> next;
-    while(node != list){
-        if(node ->data == data){
-            //删除操作
-            node -> pre -> next = node ->next;
-            node ->next ->pre = node ->pre;
-            free(node);
-            return TRUE;
-        }
-        node = node -> next;
-    }
-    return FALSE;
+void push(NODE* S, int data) {
+	NODE* node = (NODE*)malloc(sizeof(NODE));
+	node ->data = data;
+	node ->next = S ->next;
+	S ->next = node;
 }
 
-void printlist(NODE* list){
-    NODE* node = list ->next;
-    while(node!=list){
-        printf("%d -> ",node->data);
-        node = node ->next;
-    }
-    printf("NULL\n");
+
+void printstack(NODE* S) {
+	NODE* node = S ->next;
+	while (node) {
+		printf("%d -> ", node ->data);
+		node = node->next;
+	}
+	printf("NULL\n");
 }
 
 
 int main(void) {
-    NODE* list = initlist();
-    headinsert(list,1);
-    headinsert(list,2);
-    headinsert(list,3);
-    headinsert(list,4);
-    headinsert(list,0);
-    headinsert(list,5);
-    tailinsert(list,6);
-    tailinsert(list,8);
-
-    // deletelist(list,1);
-    printlist(list);
+	NODE* S = initstack();
+	push(S, 1);
+	push(S, 4);
+	push(S, 2);
+	push(S, 3);
+	printstack(S);
+	int i = pop(S);
+	printf("%d \n",i);
 	return 0;
 }
-
-
