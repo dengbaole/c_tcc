2cba45c4a6f320d78aebe39e50b60efbddfb269a
dengbaole <dengbaole155@gmail.com>
2024-08-04 23:03:56 +0800

    简单的状态机

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 
 
+format:
+	@echo Astyle source code format
+	./utils_bin/astyle.exe --project="./utils_bin/.astylerc" -r **.c,**.h --ignore-exclude-errors --exclude=_build --exclude=utils_bin --exclude=dist --exclude=utils -v -Q
+
 
 
 build:
--- main.c
+++ main.c
@@ -3,17 +3,23 @@
 #include <stdlib.h>
 #include <windows.h>
 
-struct  person {
-	int a ;
-	int b ;
-};
+#define REGS_FOREACH(_) _(X)  _(Y)
+#define RUN_LOGIC  X1 = !X && Y;Y1 = !X&&!Y;
+#define DEFINE(X) static int X,X##1;
+#define UPDATE(X) X = X##1;
+#define PRINT(X)  printf(#X " = %d ",X);
+
 
 //这里保存每个数字的值
 int main(void) {
-	struct person p1;
-	printf("hello world!\n");
-
-	return 0;
+	REGS_FOREACH(DEFINE);
+	while (1) {
+		RUN_LOGIC;
+		REGS_FOREACH(PRINT);
+		REGS_FOREACH(UPDATE);
+		putchar('\n');
+		sleep(1000);
+	}
 }
 
 
